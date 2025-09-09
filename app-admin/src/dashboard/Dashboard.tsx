5c81bb233633c9a9096e922cb3f842bfacabd7cc
diegocordova <A01781166@tec.mx>
2025-09-08 23:43:35 -0600

    Cambios para hooks useCanAccess y usePermissions

--- app-admin/src/dashboard/Dashboard.tsx
+++ app-admin/src/dashboard/Dashboard.tsx
@@ -1,15 +1,49 @@
+import { usePermissions } from "react-admin";
 import { Card, CardContent, CardHeader } from "@mui/material";
+
 import { Photos } from "./Photo";
 import { Click_Example } from "./Click_Example";
 import { Counter } from "./Counter";
 
-export const Dashboard = () => (
-    <Card>
-        <CardHeader title="Título del dashbaord" />  
-        <CardContent>
-            <Photos />
-            <Click_Example />
-            <Counter />
-        </CardContent>
-    </Card>
-);
+export const Dashboard = () => {
+    // Obtener los permisos del usuario
+    const { isPending, permissions } = usePermissions();
+
+    if (isPending) { // Indicador de carga
+        return <div>Esperando permisos...</div>;
+    } else {
+        return (
+            <Card>
+                <CardHeader title="Inicio" />
+                {permissions === 'admin' && // Administrador
+                    <CardContent>
+                        <h2>Portal de Administrador</h2>
+                        <Photos />
+                        <Click_Example />
+                        <Counter />
+                    </CardContent>
+                } 
+                {permissions === 'jefe' && // Jefe de turno
+                    <CardContent>
+                        <h2>Portal de Jefe de turno</h2>
+                    </CardContent>
+                }
+                {permissions === 'paramedico' && // Paramédico
+                    <CardContent>
+                        <h2>Portal de Emergencias Médias</h2>
+                    </CardContent>
+                }
+                {permissions === 'urbano' && // Emergencias Urbanas
+                    <CardContent>
+                        <h2>Portal de Emergencias Urbanas</h2>
+                    </CardContent>
+                }
+                {permissions === '' && // Otros
+                    <CardContent>
+                        <h2>No tienes acceso a este apartado.</h2>
+                    </CardContent>
+                }
+            </Card>
+        )
+    }
+};
